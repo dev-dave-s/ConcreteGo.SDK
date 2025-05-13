c41ba1f1ee0cb28dfbb8201b52d1c290cb4bdde4
Allon Song <allon.song@hallett.com.au>
2025-05-13 16:02:31 +0930

    Added ItemAddOrUpdate Method

---
+++ ConcreteGo.SDK.Models/Items/ItemAddOrUpdateRq.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConcreteGo.SDK.Models.Items
+{
+    [XmlRoot(ElementName = "ItemUpdate", Namespace = "")]
+    public class ItemAddOrUpdateRq
+    {
+        [XmlElement(ElementName = "ID")]
+        public int ID { get; set; }
+
+        [XmlElement(ElementName = "Code")]
+        public string Code { get; set; } = string.Empty;
+
+        [XmlElement(ElementName = "UpdateDate")]
+        public DateTime? UpdateDate { get; set; }
+
+        [XmlElement(ElementName = "Sales")]
+        public Sales Sales { get; set; } = null!;
+
+        [XmlElement(ElementName = "Mix")]
+        public Mix Mix { get; set; } = null!;
+
+        [XmlElement(ElementName = "Locations")]
+        public Locations Locations { get; set; } = null!;
+
+        //Include Ret
+        public List<string> IncludeRetElement { get; set; } = new List<string>();
+    }
+}
--- ConcreteGo.SDK/ConcreteGoClient.cs
+++ ConcreteGo.SDK/ConcreteGoClient.cs
@@ -1207,6 +1207,53 @@ namespace ConcreteGo.SDK
             return result;
         }
 
+        public async Task<List<ItemRet>?> AddOrUpdateItem(ItemAddOrUpdateRq data)
+        {
+            var requestElementName = "ItemUpdateRq";
+
+            await ManageLogin();
+            var request = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XProcessingInstruction("webcretexml", "version=\"1.0\""),
+                new XElement("WebcreteXML",
+                new XElement("WebcreteXMLMsgsRq",
+                new XElement(requestElementName, ""))));
+
+            if (request.Root != null)
+            {
+                var requestElement = request.Root.Descendants().FirstOrDefault(x => x.Name.LocalName == requestElementName);
+
+
+                var requestData = XElement.Parse(Serialize(data));
+                if (requestElement != null)
+                {
+                    requestElement.Add(requestData);
+                }
+            }
+
+            var response = new ProcessRequestResponse();
+            try
+            {
+                response = await _api.ProcessRequestAsync(_ticketHeader, request.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error processing request: " + ex.Message);
+            }
+
+            List<ItemRet>? result = null;
+            try
+            {
+                result = Deserialize<ItemRet>(response.ProcessRequestResult, "ItemUpdateRs");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error deserializing xml response: " + ex.Message);
+            }
+
+            return result;
+        }
+
         #endregion
 
         #region Item Categories
